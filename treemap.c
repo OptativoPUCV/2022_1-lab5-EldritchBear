b366995b0370c0dfe5b876c1c740264b832714c8
EldritchBear <>
2022-04-23 20:54:18 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -37,7 +37,7 @@ TreeNode * createTreeNode(void* key, void * value) {
 }
 
 TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
-  map->lower_than = lower_than
+  map->lower_than = lower_than;
     return map;
 }
 
