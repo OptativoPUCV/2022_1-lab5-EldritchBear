7df93e1e6ccb314998510e7ae0539b7cac11789d
EldritchBear <>
2022-04-23 19:00:48 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -83,7 +83,7 @@ if((node->left == NULL) && (node->right == NULL)){
     TreeNode *aux = node;
     aux = aux->left;
     while (aux->right != NULL){
-      printf("key: %d\n", *(int*)node->pair->key );
+      printf("key: %d\n", *(int*)aux->pair->key );
       aux = aux->right;
     }
     node = aux;
